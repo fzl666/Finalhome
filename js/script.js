40346ae4ad48bc4fbf49ae65c6bdba1745d81e35
Luwenxi Song <songl652@newschool.edu>
2020-12-31 01:50:43 -0500

    bg color'

--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@ var Img = [new PANOLENS.ImagePanorama("https://cors-anywhere.herokuapp.com/https
 var viewer = new PANOLENS.Viewer({container: panoImage});
 var firstViews = [new THREE.Vector3(4993.24, -95.91, -57.16),new THREE.Vector3(4993.24, 196.69, 226.84),new THREE.Vector3(4993.24, -1851.21, 155.85),new THREE.Vector3(4993.24, -1489.91, 291.18),new THREE.Vector3(4993.24, 87.05, -30000),new THREE.Vector3(4093.24, -728.76, 2167.66),new THREE.Vector3(4093.24, 546.90, 4630.54),new THREE.Vector3(4607.14, -263.59, -1902.15),new THREE.Vector3(4631.77, 505.89, 1786.42),new THREE.Vector3(4943.96, -683.51, -117.58),new THREE.Vector3(4934.71, 55.19, -748.06),new THREE.Vector3(4859.42, 83.32, -1143.80),new THREE.Vector3(4863.52, 1021.00, -515.17),new THREE.Vector3(218.72, 799.49, -4923.53)]
 var infospotPositions = [new THREE.Vector3(3877.12, -1788.91, -2583.93),new THREE.Vector3(2420.21, -3808.29, -2151.52),new THREE.Vector3(2900.74, -2625.62, -3107.69),new THREE.Vector3(2108.34, -3034.84, -3311.21),new THREE.Vector3(-4763.47, -1485.28, -165.66),new THREE.Vector3(-3001.20, -2266.50, -3292.79),new THREE.Vector3(2074.2, -2351.61, 3885.02),new THREE.Vector3(-2936.56, -461.82, 4016.62),new THREE.Vector3(3393.62, -611.24, 3609.39),new THREE.Vector3(4013.12, -1408.51, -2610.07),new THREE.Vector3(-4231.53, -571.92, -2584.58),new THREE.Vector3(4046.32, -2923.57, 172.21),new THREE.Vector3(-3589.10, -2469.72, 2436.72),new THREE.Vector3(1799.14, -395.36, 4640.89),new THREE.Vector3(3905.18, 183.94, -3100.23),new THREE.Vector3(-4113.25, 99.24, -2826.81),new THREE.Vector3(4896.95, 98.53, -975.64),new THREE.Vector3(1325.78, 164.78, -4807.35),new THREE.Vector3(181 -2535.27, 92.61, -4302.89),new THREE.Vector3(181 -4902.97, 218.70, 907.50)]
-console.log(Img.length-1)
-console.log(firstViews.length-1)
-console.log(infospotPositions.length-1)
 
 function myFunction() {
     var x = document.getElementById("shortcuts");
